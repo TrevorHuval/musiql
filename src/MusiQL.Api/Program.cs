05de1d7ce9488f017194577316df0491306a27ad
TrevorHuval <huvaltrevor@gmail.com>
2026-07-20 09:32:11 -0500

    allow the 127.0.0.1 dev origin for the spotify callback

--- src/MusiQL.Api/Program.cs
+++ src/MusiQL.Api/Program.cs
@@ -99,7 +99,7 @@ builder.Services.AddRateLimiter(options =>
 const string devCors = "dev";
 builder.Services.AddCors(options =>
     options.AddPolicy(devCors, policy => policy
-        .WithOrigins("http://localhost:5173")
+        .WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
         .AllowAnyHeader()
         .AllowAnyMethod()));
 
